20ac9cec0ad82cee6c9f082422ebf5ac589972fb
dea fr <procesarleon@gmail.com>
2022-02-18 13:48:49 -0800

    Add files via upload
    Leet code numerals challenge

---
+++ roman_numeral.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+int main() {
+string s;
+  cin >> s;
+unordered_map<char,int> T = { {'I', 1},
+  {'V',5},{'X',10}, {'L', 50},{'C',100},{'D',500},{'M',1000}};
+  
+ // std::cout << "Hello World!\n";
+
+int sum = T[s.back()];
+  
+ // for(int i = 0; i < T.size()/4;i++)
+  //for(int i = (T.size()/4);i >=0; i--)
+  for(int i =s.length() -2; i >=0;i--)
+     //for( auto& t : T)
+       //cout << t.first << " " << t.second <<endl;
+  
+       if (T[s[i]] < T[s[i + 1]])
+       {
+           sum -= T[s[i]];
+       }
+       else
+       {
+           sum += T[s[i]];
+       }
+   
+   
+   return sum;
+}
