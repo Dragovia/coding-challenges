43e19763d7fe9ad45798c4eb2b04dd9dc7b412c9
Cesar Leon <procesarleon@gmail.com>
2024-01-18 23:53:21 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,56 +1,37 @@
+// Insertion sort in C++
+
 #include <iostream>
-#include <bits/stdc++.h>
 using namespace std;
 
-
-
-
-void selectionSort(int arr[], int n)
-{
-    int i, j, min_idx;
-
-    // One by one move boundary of
-    // unsorted subarray
-    for (i = 0; i < n - 1; i++) {
-      ///0 to the last element in array forloop
-        // Find the minimum element in
-        // unsorted array
-        min_idx = i; /// mid is the first element
-        for (j = i + 1; j < n; j++) {
-                ///second element to the end
-            if (arr[j] < arr[min_idx])
-            /// if the element on the right is smaller
-            ///than the element on the left
-                min_idx = j;
-           /// then the index on the right becomes the new min index
-        }
-
-        // Swap the found minimum element
-        // with the first element
-        if (min_idx != i) ///min_indx isn't
-            swap(arr[min_idx], arr[i]);
-    }
-}
-
 // Function to print an array
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-        cout << endl;
-    }
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
 }
 
-// Driver program
-int main()
-{
-    int arr[] = { 64, 25, 12, 22, 11 };
-    int n = sizeof(arr) / sizeof(arr[0]);
+void insertionSort(int array[], int size) {
+  for (int step = 1; step < size; step++) {
+    int key = array[step];
+    int j = step - 1;
 
-    // Function Call
-    selectionSort(arr, n);
-    cout << "Sorted array: \n";
-    printArray(arr, n);
-    return 0;
+    // Compare key with each element on the left of it until an element smaller than
+    // it is found.
+    // For descending order, change key<array[j] to key>array[j].
+    while (key < array[j] && j >= 0) {
+      array[j + 1] = array[j];
+      --j;
+    }
+    array[j + 1] = key;
+  }
+}
+
+// Driver code
+int main() {
+  int data[] = {9, 5, 1, 4, 3};
+  int size = sizeof(data) / sizeof(data[0]);
+  insertionSort(data, size);
+  cout << "Sorted array in ascending order:\n";
+  printArray(data, size);
 }
