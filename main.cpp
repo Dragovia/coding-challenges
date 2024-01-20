c63d6fb6f526167250442f59087a1a6571c70965
Cesar Leon <procesarleon@gmail.com>
2024-01-19 23:51:25 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,37 +1,37 @@
-// Insertion sort in C++
-
 #include <iostream>
+#include <stack>
+
 using namespace std;
 
-// Function to print an array
-void printArray(int array[], int size) {
-  for (int i = 0; i < size; i++) {
-    cout << array[i] << " ";
-  }
-  cout << endl;
-}
+int main()
+{
+    stack<int> staq;
 
-void insertionSort(int array[], int size) {
-  for (int step = 1; step < size; step++) {
-    int key = array[step];
-    int j = step - 1;
+    staq.push(21);
+    staq.push(22);
+    staq.push(24);
+    staq.push(25);
 
-    // Compare key with each element on the left of it until an element smaller than
-    // it is found.
-    // For descending order, change key<array[j] to key>array[j].
-    while (key < array[j] && j >= 0) {
-      array[j + 1] = array[j];
-      --j;
+    int num = 0;
+    staq.push(num);
+
+    while(!staq.empty()){
+        cout << staq.top() << " ";
+        staq.pop();
     }
-    array[j + 1] = key;
-  }
-}
 
-// Driver code
-int main() {
-  int data[] = {9, 5, 1, 4, 3};
-  int size = sizeof(data) / sizeof(data[0]);
-  insertionSort(data, size);
-  cout << "Sorted array in ascending order:\n";
-  printArray(data, size);
+    //staq.pop();
+    //staq.pop();
+cout << endl;
+   while(!staq.empty()){
+         cout << staq.top() << " ";
+            //staq.pop();
+         }
+
+   if(staq.empty() )
+    cout << "cheese";
+
+
+
+    return 0;
 }
