d6d74f9fef50592abea0949d37b41f93b5e77384
Cesar Leon <procesarleon@gmail.com>
2025-01-10 23:28:14 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,35 +3,105 @@
 
 using namespace std;
 
-int main()
-{
-    stack<int> staq;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
 
-    staq.push(21);
-    staq.push(22);
-    staq.push(24);
-    staq.push(25);
+    Node(int value) : data(value), left(nullptr), right(nullptr) {}
+};
 
-    int num = 0;
-    staq.push(num);
+class BST {
+public:
+    Node* root;
 
-    while(!staq.empty()){
-        cout << staq.top() << " ";
-        staq.pop();
+    BST() : root(nullptr) {}
+
+    void insert(int data) {
+        Node* newNode = new Node(data);
+        if (root == nullptr) {
+            root = newNode;
+            return;
+        }
+
+        Node* current = root;
+        Node* parent = nullptr;
+
+        while (current != nullptr) {
+            parent = current;
+            if (data < current->data) {
+                current = current->left;
+            } else {
+                current = current->right;
+            }
+        }
+
+        if (data < parent->data) {
+            parent->left = newNode;
+        } else {
+            parent->right = newNode;
+        }
     }
 
-    //staq.pop();
-    //staq.pop();
-cout << endl;
-   while(!staq.empty()){
-         cout << staq.top() << " ";
-            //staq.pop();
-         }
+    void inorder() {
+        if (root == nullptr) return;
 
-   if(staq.empty() )
-    cout << "cheese";
+        stack<Node*> s;
+        Node* current = root;
 
+        while (current != nullptr || !s.empty()) {
+            while (current != nullptr) {
+                s.push(current);
+                current = current->left;
+            }
 
+            current = s.top();
+            s.pop();
+
+            cout << current->data << " ";
+
+            current = current->right;
+        }
+    }
+
+    Node* search(int key) {
+        Node* current = root;
+
+        while (current != nullptr && current->data != key) {
+            if (key < current->data) {
+                current = current->left;
+            } else {
+                current = current->right;
+            }
+        }
+
+        return current;  // Will be nullptr if key not found
+    }
+};
+
+int main() {
+    BST tree;
+
+    tree.insert(50);
+    tree.insert(30);
+    tree.insert(70);
+    tree.insert(20);
+    tree.insert(40);
+    tree.insert(60);
+    tree.insert(80);
+
+    cout << "Inorder traversal: ";
+    tree.inorder();
+    cout << endl;
+
+    int key = 40;
+    Node* found = tree.search(key);
+    if (found != nullptr) {
+        cout << "Node with value " << key << " found in the tree." << endl;
+    } else {
+        cout << "Node with value " << key << " not found in the tree." << endl;
+    }
 
     return 0;
 }
+
